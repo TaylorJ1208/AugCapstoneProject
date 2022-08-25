a66c368842e8c83651324b8b86fcb422993c57da
BlaiseHarris <baharris5@my.waketech.edu>
2022-08-25 16:04:15 -0400

    Checkout shows current inputted information before button

--- src/app/checkout-page/checkout-page.component.ts
+++ src/app/checkout-page/checkout-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-checkout-page',
@@ -6,10 +6,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout-page.component.scss']
 })
 export class CheckoutPageComponent implements OnInit {
+  inputName: string = "";
+  inputAddress: string = "";
+  inputCity: string = "";
+  inputAptNo: string = "";
+  inputState: string = "";
+  inputZipcode: string = "";
+  inputCountry: string = "";
+  inputCardNum: string = "";
+  inputExpDate: string = "";
+  inputCVV: string = "";
+  inputOwner: string = "";
 
   constructor() { }
-
+  
   ngOnInit(): void {
+    
+  }
+
+  changedName(event : any){
+    this.inputName = event.target.value;
+  }
+  
+  changedAddress(event : any){
+    this.inputAddress = event.target.value;
+  }
+
+  changedCity(event : any){
+    this.inputCity = event.target.value;
+  }
+
+  changedAptNo(event : any){
+    this.inputAptNo = event.target.value;
+  }
+
+  changedState(event : any){
+    this.inputState = event.target.value;
+  }
+
+  changedCountry(event : any){
+    this.inputCountry = event.target.value;
+  }
+
+  changedZipcode(event : any){
+    this.inputZipcode = event.target.value;
+  }
+
+  changedOwner(event : any){
+    this.inputOwner = event.target.value;
+  }
+
+  changedCardNum(event : any){
+    this.inputCardNum = event.target.value;
+  }
+
+  changedExpDate(event : any){
+    this.inputExpDate = event.target.value;
+  }
+
+  changedCVV(event : any){
+    this.inputCVV = event.target.value;
   }
 
 }
