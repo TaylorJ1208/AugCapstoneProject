74bb9d4c1bf125e9ce52de8335378296c023190f
Taylor Joostema <taylor.joostema@hcl.com>
2022-08-25 12:40:12 -0400

    Fixed home-page not showing data

--- src/app/category-nav/category-nav.component.ts
+++ src/app/category-nav/category-nav.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavService } from '../services/nav-service/nav-service/nav.service';
+import { ProductService } from '../services/product-service/product.service';
+import { Product } from '../Models/product';
 
 @Component({
   selector: 'app-category-nav',
@@ -7,14 +9,24 @@ import { NavService } from '../services/nav-service/nav-service/nav.service';
   styleUrls: ['./category-nav.component.scss']
 })
 export class CategoryNavComponent implements OnInit {
-
-  constructor(private navService: NavService) { }
+  products: Product[] = [];
+  constructor(private navService: NavService, private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.getProducts();
   }
 
   filterCategory(e: Event): void {
     this.navService.getProductByCategory((e.target as HTMLButtonElement).value);
   }
+
+  getProducts() {
+    this.productService.getAllProducts()
+      .subscribe({ next: (data: Product[]) => {
+        console.log(data);
+        this.products = data;
+      },
+      error: (e) => console.error(e)});
+    }
 
 }
