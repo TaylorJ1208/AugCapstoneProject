198ecbca98d5c9bdc0dcd7f9db3e913a4e93db43
Taylor <TaylorJ1208@yahoo.com>
2022-09-13 09:56:20 -0400

    Changes test main

--- src/test/java/com/ecommerce/EcommerceBackendApplicationTests.java
+++ src/test/java/com/ecommerce/EcommerceBackendApplicationTests.java
@@ -1,15 +1,8 @@
 package com.ecommerce;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EcommerceBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-		assertTrue(true);
-	}
 }
