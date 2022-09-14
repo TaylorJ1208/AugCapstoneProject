36410ff9a4e9ea873ec1bd628fbc374324ea7f6c
BlaiseHarris <baharris5@my.waketech.edu>
2022-09-14 10:48:18 -0400

    Messages save in queue

--- src/main/java/com/ecommerce/config/RabbitConfig.java
+++ src/main/java/com/ecommerce/config/RabbitConfig.java
@@ -1,13 +1,8 @@
 package com.ecommerce.config;
 
 import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.ecommerce.rabbitmq.Receiver;
 
 @Configuration
 public class RabbitConfig {
@@ -29,20 +24,5 @@ public class RabbitConfig {
 	  @Bean
 	  Binding binding(Queue queue, TopicExchange exchange) {
 	    return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
-	  }
-
-	  @Bean
-	  SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-	      MessageListenerAdapter listenerAdapter) {
-	    SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-	    container.setConnectionFactory(connectionFactory);
-	    container.setQueueNames(QUEUE);
-	    container.setMessageListener(listenerAdapter);
-	    return container;
-	  }
-
-	  @Bean
-	  MessageListenerAdapter listenerAdapter(Receiver receiver) {
-	    return new MessageListenerAdapter(receiver, "receiveMessage");
 	  }
 }
--- src/main/java/com/ecommerce/controller/RestockController.java
+++ src/main/java/com/ecommerce/controller/RestockController.java
@@ -1,7 +1,6 @@
 package com.ecommerce.controller;
 
-import java.util.concurrent.TimeUnit;
-
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.config.RabbitConfig;
 import com.ecommerce.model.Product;
-import com.ecommerce.rabbitmq.Receiver;
 import com.ecommerce.service.ProductService;
 
 @RestController
@@ -21,21 +19,18 @@ public class RestockController {
 	@Autowired
 	private ProductService productService;
 	
-	private final RabbitTemplate rabbitTemplate;
-	private final Receiver receiver;
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+	
+	@Autowired
+	private TopicExchange exchange;
 
-
-	public RestockController(Receiver receiver, RabbitTemplate rabbitTemplate) {
-		this.receiver = receiver;
-		this.rabbitTemplate = rabbitTemplate;
-	}
 	
 	@GetMapping("/restock/remind/{id}")
 	public void restockMessage(@PathVariable Long id) throws InterruptedException {
 		Product product = productService.getProductById(id);
 		String send = "The product: " + product.getName() + " is out-of-stock. Please request restock from " + product.getVendors().getEmail();
-		rabbitTemplate.convertAndSend(RabbitConfig.EXCHANGE, RabbitConfig.ROUTING_KEY, send);
-		receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+		rabbitTemplate.convertAndSend(exchange.getName(), RabbitConfig.ROUTING_KEY, send);
 	}
 
 }
