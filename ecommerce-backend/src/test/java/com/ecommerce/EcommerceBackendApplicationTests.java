90f6ba006c4f3b5d7b9925d619af1346c99c5f8d
Taylor <TaylorJ1208@yahoo.com>
2022-09-14 14:32:56 -0400

    Removes failing test in src/test

--- ecommerce-backend/src/test/java/com/ecommerce/EcommerceBackendApplicationTests.java
+++ ecommerce-backend/src/test/java/com/ecommerce/EcommerceBackendApplicationTests.java
@@ -1,16 +1,9 @@
 package com.ecommerce;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EcommerceBackendApplicationTests {
 	
-	@Test
-	void testStartup() {
-		assertTrue(true);
-	}
 
 }
