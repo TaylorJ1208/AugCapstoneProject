07c95f45543b8a098343b5575fdae9f5f71510a9
TJ1208 <71390658+TJ1208@users.noreply.github.com>
2022-09-20 16:23:36 -0400

    Update my-app-config.ts

--- ecommerce-frontend/src/app/config/my-app-config.ts
+++ ecommerce-frontend/src/app/config/my-app-config.ts
@@ -2,8 +2,8 @@ export default {
     oidc: {
         clientId: '0oa6hnwwonsOm1f6n5d7',
         issuer: 'https://dev-3553715.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/home',
-        postLogoutRedirectUri: 'http://localhost:4200/login',
+        redirectUri: 'https://e-backend.azurewebsites.net/home',
+        postLogoutRedirectUri: 'https://e-backend.azurewebsites.net/login',
         scopes: ['openid', 'profile', 'email']
     }
 }
