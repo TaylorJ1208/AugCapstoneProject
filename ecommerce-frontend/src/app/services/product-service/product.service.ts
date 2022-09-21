d947bfdefcdc6250456d518f2e3e01562f436e4e
henry nguyen <huynguyen2741@yahoo.com>
2022-09-21 09:30:35 -0400

    fix product.service link

--- ecommerce-frontend/src/app/services/product-service/product.service.ts
+++ ecommerce-frontend/src/app/services/product-service/product.service.ts
@@ -4,36 +4,46 @@ import { Observable } from 'rxjs';
 import { Product } from 'src/app/Models/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getAllProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>("https://e-backend.azurewebsites.net/catalog/customer");
-
+    return this.http.get<Product[]>(
+      'https://e-backend.azurewebsites.net/catalog/customer'
+    );
   }
 
   getProductByName(name: string): Observable<Product[]> {
-    return this.http.get<Product[]>(`http://e-backend.azurewebsites.net/catalog/customer/product/${name}`);
+    return this.http.get<Product[]>(
+      `https://e-backend.azurewebsites.net/catalog/customer/product/${name}`
+    );
   }
 
-  
   getProductById(id: any): Observable<any> {
-    return this.http.get<any>(`http://e-backend.azurewebsites.net/catalog/customer/${id}`);
+    return this.http.get<any>(
+      `https://e-backend.azurewebsites.net/catalog/customer/${id}`
+    );
   }
 
   deleteProduct(id: any): Observable<any> {
-    return this.http.delete(`http://e-backend.azurewebsites.net/catalog/admin/delete/${id}`);
+    return this.http.delete(
+      `https://e-backend.azurewebsites.net/catalog/admin/delete/${id}`
+    );
   }
 
-  updateProduct(data:any):Observable<any>{
-    return this.http.put(`http://e-backend.azurewebsites.net/catalog/admin/update`,data);
+  updateProduct(data: any): Observable<any> {
+    return this.http.put(
+      `https://e-backend.azurewebsites.net/catalog/admin/update`,
+      data
+    );
   }
 
-  addProduct(data:any):Observable<any>{
-    return this.http.post(`http://e-backend.azurewebsites.net/catalog/admin/add`,data);
+  addProduct(data: any): Observable<any> {
+    return this.http.post(
+      `https://e-backend.azurewebsites.net/catalog/admin/add`,
+      data
+    );
   }
-
 }
