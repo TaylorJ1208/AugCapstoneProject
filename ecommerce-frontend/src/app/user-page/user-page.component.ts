0a1c237fc533d2491bf4259972241a55e543544b
Taylor <TaylorJ1208@yahoo.com>
2022-09-22 11:35:23 -0400

    changes user-page functionality for signed in user

--- ecommerce-frontend/src/app/user-page/user-page.component.ts
+++ ecommerce-frontend/src/app/user-page/user-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { CheckboxControlValueAccessor } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ngbCarouselTransitionOut } from '@ng-bootstrap/ng-bootstrap/carousel/carousel-transition';
 import { OktaAuthService } from '@okta/okta-angular';
 import { User } from '../Models/user';
 import { UserService } from '../services/user-service/user.service';
@@ -18,11 +20,16 @@ export class UserPageComponent {
   constructor(private router: Router, private oktaAuthService: OktaAuthService, private userService: UserService) {
     this.oktaAuthService.getUser().then((user) => {
       this.sub = user.sub;
-      this.userService.getUserByOktaId(this.sub).subscribe((user) => {
+      this.validateUser(this.sub);
+    });
+  }
+
+
+    validateUser(sub: string) {
+      this.userService.getUserByOktaId(sub).subscribe((user) => {
         this.user = user;
         this.isUser = this.user?.roles[0].role == "ROLE_USER" ? true : false;
       });
-    });
-  }
+    }
 }
 
